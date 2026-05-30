a8453974874c211a7df3c0425301efb10f8b115c
Yuvraj Singh Chauhan <76736925+yuvraj091020@users.noreply.github.com>
2026-05-31 03:36:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    from waitress import serve
+
+    serve(app, host="0.0.0.0", port=8080)
+    app.run(debug=True)
